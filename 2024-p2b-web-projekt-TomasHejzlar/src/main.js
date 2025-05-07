358319a099274703a32c74e8a1269c4ddb815613
TomasHejzlar <tomas.hejzlar.023@pslib.cz>
2025-05-07 21:39:35 +0200

    lightgallery pridano

--- 2024-p2b-web-projekt-TomasHejzlar/src/main.js
+++ 2024-p2b-web-projekt-TomasHejzlar/src/main.js
@@ -1,2 +1,14 @@
 import './style.css'
 import './normalize.css'
+import lightGallery from 'lightgallery';
+import 'lightgallery/css/lightgallery.css';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const gallery = document.getElementById('gallery');
+  if (gallery) {
+    lightGallery(gallery, {
+      selector: 'a',
+    });
+  }
+});
